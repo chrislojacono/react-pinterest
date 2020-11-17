e49520ee6e4f2eb369db8ac08e89019c5b663c6d
Chris LoJacono <clojacono92@gmail.com>
2020-11-17 13:54:23 -0600

    users pins in pins section

--- src/helpers/data/pinData.js
+++ src/helpers/data/pinData.js
@@ -28,15 +28,28 @@ const getSinglePin = (pinId) => new Promise((resolve, reject) => {
 const deletePin = (pinUid) => axios.delete(`${baseUrl}/Pins/${pinUid}.json`);
 
 const createPin = (object) => new Promise((resolve, reject) => {
-  axios.post(`${baseUrl}/Pinss.json`, object)
+  axios.post(`${baseUrl}/Pins.json`, object)
     .then((response) => {
-      axios.patch(`${baseUrl}/Pinss/${response.data.name}.json`, { firebaseKey: response.data.name }).then(resolve);
+      axios.patch(`${baseUrl}/Pins/${response.data.name}.json`, { firebaseKey: response.data.name }).then(resolve);
     }).catch((error) => reject(error));
 });
 
 const updatePin = (object) => new Promise((resolve, reject) => {
-  axios.patch(`${baseUrl}/Pinss/${object.firebaseKey}.json`, object)
+  axios.patch(`${baseUrl}/Pins/${object.firebaseKey}.json`, object)
     .then(resolve).catch((error) => reject(error));
+});
+
+const getUserPins = (userId) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/Pins.json?orderBy="UserId"&equalTo="${userId}"`).then((response) => {
+    const pinResponse = response.data;
+    const pinArray = [];
+    if (pinResponse) {
+      Object.keys(pinResponse).forEach((pin) => {
+        pinArray.push(pinResponse[pin]);
+      });
+    }
+    resolve(pinArray);
+  }).catch((error) => reject(error));
 });
 
 export {
@@ -45,4 +58,5 @@ export {
   deletePin,
   updatePin,
   createPin,
+  getUserPins,
 };
--- src/views/pins.js
+++ src/views/pins.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import getUid from '../helpers/data/authData';
+import { getUserPins } from '../helpers/data/pinData';
+import PinCard from '../components/Cards/PinCard';
 
-export default function Pins() {
-  return (
-    <div>
-      <h1>Pin View</h1>
-    </div>
-  );
+export default class Pins extends React.Component {
+  state = {
+    pins: [],
+  };
+
+  componentDidMount() {
+    const userId = getUid();
+    getUserPins(userId).then((response) => {
+      this.setState({
+        pins: response,
+      });
+    });
+  }
+
+  render() {
+    const { pins } = this.state;
+    return (
+      <div>
+        <h1>Your Pins</h1>
+        <div className="d-flex flex-wrap container">
+        {pins.map((pin) => <PinCard key={pin.firebaseKey} pinData={pin} />)}
+        </div>
+
+      </div>
+    );
+  }
 }
