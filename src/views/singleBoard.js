fa523cada8fd101bdab6109c5d0dce594163c73a
Chris LoJacono <clojacono92@gmail.com>
2020-11-17 17:40:45 -0600

    fix

--- src/views/singleBoard.js
+++ src/views/singleBoard.js
@@ -12,14 +12,9 @@ export default class SingleBoard extends React.Component {
   };
 
   componentDidMount() {
-    // 1. Pull boardId from URL params
     const boardId = this.props.match.params.id;
-
     this.getBoardInfo(boardId);
-    this.getPins(boardId)
-      .then((resp) => (
-        this.setState({ pins: resp })
-      ));
+    this.getPins(boardId).then((resp) => this.setState({ pins: resp }));
   }
 
   getBoardInfo = (boardId) => {
@@ -28,35 +23,29 @@ export default class SingleBoard extends React.Component {
         board: response,
       });
     });
-  }
+  };
 
-  getPins = (boardId) => (
+  getPins = (boardId) => {
     getBoardPins(boardId).then((response) => {
-      const pinArray = [];
-      response.forEach((item) => {
       const pinArray = [];
       response.forEach((item) => {
         pinArray.push(getSinglePin(item.pinId));
       });
       return Promise.all([...pinArray]);
-    })
-  )
+    });
+  }
 
   render() {
     const { pins, board } = this.state;
-    const renderPins = () => (
-      pins.map((pin) => (
-        <PinCard key={pin.firebaseKey} pinData={pin} />
-      ))
-    );
+    const renderPins = () => pins.map((pin) => <PinCard key={pin.firebaseKey} pinData={pin} />);
 
     return (
       <div>
-        { Object.keys(board).length && <BoardForm board={board} onUpdate={this.getBoardInfo} />}
+        {Object.keys(board).length && (
+          <BoardForm board={board} onUpdate={this.getBoardInfo} />
+        )}
         <h1>{board.name}</h1>
-        <div className='d-flex flex-wrap container'>
-          {renderPins()}
-        </div>
+        <div className='d-flex flex-wrap container'>{renderPins()}</div>
       </div>
     );
   }
